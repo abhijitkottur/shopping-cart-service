d224cf3499d970bba32dfe718d6e255c769655aa
Abhijit Kottur <abhijit.kottur@winning.com.au>
2018-07-12 10:31:14 +1000

    naming server

--- src/main/java/com/abhijit/shoppingcartservice/ShoppingCartServiceApplication.java
+++ src/main/java/com/abhijit/shoppingcartservice/ShoppingCartServiceApplication.java
@@ -2,8 +2,10 @@ package com.abhijit.shoppingcartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @EnableFeignClients("com.abhijit.shoppingcartservice")
 @SpringBootApplication
 public class ShoppingCartServiceApplication {
