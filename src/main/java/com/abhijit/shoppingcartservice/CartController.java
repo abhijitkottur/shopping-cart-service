8743a6067aab5db1484ce52e863963fd92b05da9
Abhijit Kottur <abhijit.kottur@winning.com.au>
2018-07-22 10:25:32 +1000

    zipkin changes

--- src/main/java/com/abhijit/shoppingcartservice/CartController.java
+++ src/main/java/com/abhijit/shoppingcartservice/CartController.java
@@ -2,6 +2,9 @@ package com.abhijit.shoppingcartservice;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CartController {
 
+	private static final Logger log = LoggerFactory.getLogger(CartController.class);
+	
 	@Autowired
 	private CartService cartService;
 	
 	@GetMapping(value = "/cart/total", produces = MediaType.APPLICATION_JSON_VALUE)
 	public CartTotalResponse calculateCartTotal(@RequestParam List<String> bookIds) {
+		log.info("Cart requested with books {}", StringUtils.join(bookIds, ","));
 		return cartService.calculateCartTotal(bookIds);
 	}
 	
--- src/main/java/com/abhijit/shoppingcartservice/ShoppingCartServiceApplication.java
+++ src/main/java/com/abhijit/shoppingcartservice/ShoppingCartServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @EnableEurekaClient
 @EnableFeignClients("com.abhijit.shoppingcartservice")
@@ -12,5 +15,10 @@ public class ShoppingCartServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingCartServiceApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
